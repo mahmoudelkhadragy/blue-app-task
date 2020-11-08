ec0fc26275f71fe3522e966e7fccd49fea244053
unknown <mahmoudelkhadragy38@gmail.com>
2020-11-08 10:32:34 +0200

    change favicon and add data

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,73 +1,26 @@
 import React from "react";
+import { data } from "../data";
 
 function Products() {
+  const { products } = data;
   return (
     <div className="products">
       <div className="container">
         <div className="row">
-          <div className="col-12 col-md-3 col-sm-6">
-            <div className="product">
-              <div className="product_img">
-                <img src="images/products/product1.jpg" alt="product-img" />
-              </div>
-              <div className="product_info">
-                <h3 className="product_name">Chair</h3>
-                <span className="product_price">55$</span>
-                <p className="product_desc">
-                  Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
-                </p>
-              </div>
-            </div>
-          </div>
-
-          <div className="col-12 col-md-3 col-sm-6">
-            <div className="product">
-              <div className="product_img">
-                <img src="images/products/product1.jpg" alt="product-img" />
-              </div>
-              <div className="product_info">
-                <h3 className="product_name">Chair</h3>
-                <span className="product_price">55$</span>
-                <p className="product_desc">
-                  Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
-                </p>
+          {products.map((product) => (
+            <div key={product._id} className="col-12 col-md-3 col-sm-6">
+              <div className="product">
+                <div className="product_img">
+                  <img src={product.image} alt="product-img" />
+                </div>
+                <div className="product_info">
+                  <h3 className="product_name">{product.name}</h3>
+                  <span className="product_price">{product.price}</span>
+                  <p className="product_desc">{product.description}</p>
+                </div>
               </div>
             </div>
-          </div>
-
-          <div className="col-12 col-md-3 col-sm-6">
-            <div className="product">
-              <div className="product_img">
-                <img src="images/products/product1.jpg" alt="product-img" />
-              </div>
-              <div className="product_info">
-                <h3 className="product_name">Chair</h3>
-                <span className="product_price">55$</span>
-                <p className="product_desc">
-                  Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
-                </p>
-              </div>
-            </div>
-          </div>
-
-          <div className="col-12 col-md-3 col-sm-6">
-            <div className="product">
-              <div className="product_img">
-                <img src="images/products/product1.jpg" alt="product-img" />
-              </div>
-              <div className="product_info">
-                <h3 className="product_name">Chair</h3>
-                <span className="product_price">55$</span>
-                <p className="product_desc">
-                  Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
-                </p>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
