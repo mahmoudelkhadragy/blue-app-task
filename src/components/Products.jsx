c8f9541d3e974f8b0add36a66e53565cb3f2e40b
unknown <mahmoudelkhadragy38@gmail.com>
2020-11-08 22:04:16 +0200

    fetch data from api by axios and use toastify

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,17 +1,48 @@
-import React from "react";
-import { data } from "../data";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { toast } from "react-toastify";
 import Product from "./Product";
+import "react-toastify/dist/ReactToastify.css";
+import LoadingBox from "./LoadingBox";
+import MessageBox from "./MessageBox";
+
+toast.configure();
 
 function Products() {
-  const { products } = data;
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const { data } = await axios.get("/api/products");
+        setProducts(data);
+        setLoading(false);
+      } catch (err) {
+        setError(err.message);
+        toast.error("Something went Wrong!");
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="products">
       <div className="container">
-        <div className="row">
-          {products.map((product) => (
-            <Product key={product._id} product={product} />
-          ))}
-        </div>
+        {loading ? (
+          <LoadingBox></LoadingBox>
+        ) : error ? (
+          <MessageBox variant="danger">{error}</MessageBox>
+        ) : (
+          <div className="row">
+            {products.map((product) => (
+              <Product key={product._id} product={product} />
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
