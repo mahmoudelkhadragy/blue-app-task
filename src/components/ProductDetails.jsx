714802dd18e3d0fb4aa1beb20df03897559f450f
unknown <mahmoudelkhadragy38@gmail.com>
2020-11-11 10:50:30 +0200

    refactore productDetail component

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,62 +1,78 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import { data } from "../data";
+import { detailsProduct } from "../actions/productsActions";
+import LoadingBox from "./LoadingBox";
+import MessageBox from "./MessageBox";
 import Rating from "./Rating";
 
 function ProductDetails(props) {
-  const product = data.products.find((x) => x._id === props.match.params.id);
-  if (!product) {
-    return (
-      <div className="container text-center ">
-        <h3>Sorry Product Not Found</h3>
-      </div>
-    );
-  }
+  const dispatch = useDispatch();
+  const productId = props.match.params.id;
+  const ProductDetails = useSelector((state) => state.productDetails);
+  const { loading, error, product } = ProductDetails;
+
+  useEffect(() => {
+    dispatch(detailsProduct(productId));
+  }, [dispatch, productId]);
+
   return (
-    <div className="container py-5">
-      <div className="row">
-        <div className="col-12 col-md-5 col-sm-6">
-          <div className="product_details_img">
-            <img className="image-fluid" src={product.image} alt="product" />
-          </div>
-        </div>
-        <div className="col-12 col-md-4 col-sm-6">
-          <div className="product_details_info">
-            <h2 className="product_details_name">{product.name}</h2>
-            <Rating rating={4} numReviews={product.numReviews} />
-            <p className="product_price">
-              <span>Price:</span> {product.price}$
-            </p>
-            <p className="product_price">
-              <span>Description:</span> {product.description}
-            </p>
-          </div>
-        </div>
-        <div className="col-12 col-md-3 col-sm-6">
-          <div className="product_details_cart">
-            <div className="card_product_price">
-              <h5>Price:</h5>
-              <p>${product.price}</p>
-            </div>
-            <div className="card_product_status">
-              <h5>Status</h5>
-              <div>
-                {product.countInStock > 0 ? (
-                  <span className="success">In Stock</span>
-                ) : (
-                  <span className="error">Unavailable</span>
-                )}
+    <div className="container ">
+      {loading ? (
+        <LoadingBox></LoadingBox>
+      ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
+        <div className="container py-5">
+          <div className="row">
+            <div className="col-12 col-md-5 col-sm-6">
+              <div className="product_details_img">
+                <img
+                  className="image-fluid"
+                  src={product.image}
+                  alt="product"
+                />
               </div>
             </div>
-            <button className="btn btn-primary btn_addToCart">
-              Add to Cart
-            </button>
+            <div className="col-12 col-md-4 col-sm-6">
+              <div className="product_details_info">
+                <h2 className="product_details_name">{product.name}</h2>
+                <Rating rating={4} numReviews={product.numReviews} />
+                <p className="product_price">
+                  <span>Price:</span> {product.price}$
+                </p>
+                <p className="product_price">
+                  <span>Description:</span> {product.description}
+                </p>
+              </div>
+            </div>
+            <div className="col-12 col-md-3 col-sm-6">
+              <div className="product_details_cart">
+                <div className="card_product_price">
+                  <h5>Price:</h5>
+                  <p>${product.price}</p>
+                </div>
+                <div className="card_product_status">
+                  <h5>Status</h5>
+                  <div>
+                    {product.countInStock > 0 ? (
+                      <span className="success">In Stock</span>
+                    ) : (
+                      <span className="error">Unavailable</span>
+                    )}
+                  </div>
+                </div>
+                <button className="btn btn-primary btn_addToCart">
+                  Add to Cart
+                </button>
+              </div>
+            </div>
           </div>
+          <Link className="btn btn-secondary btn-lg mt-4" to="/">
+            Back To Products
+          </Link>
         </div>
-      </div>
-      <Link className="btn btn-secondary btn-lg mt-4" to="/">
-        Back To Products
-      </Link>
+      )}
     </div>
   );
 }
